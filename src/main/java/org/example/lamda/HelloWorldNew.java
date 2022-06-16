5b535508c2435f567faf4908d000f537ce15b7a7
johnpaul <iamjohnpaults@gmail.com>
2022-06-16 20:03:03 +0530

    Hello Lamda simplified

--- src/main/java/org/example/lamda/HelloWorldNew.java
+++ src/main/java/org/example/lamda/HelloWorldNew.java
@@ -2,9 +2,7 @@ package org.example.lamda;
 
 public class HelloWorldNew {
     public static void main(String[] args) {
-        HelloLamda helloLamda = () -> {
-            return "Hey New";
-        };
+        HelloLamda helloLamda = () -> "Hey New";
 
         System.out.println(helloLamda.sayHello());
     }
